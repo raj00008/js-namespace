59abc922a2c82f81c02b32b0c1f7d858da520ea5
raj00008 <32398051+raj00008@users.noreply.github.com>
2017-10-15 18:53:17 -0400

    final commit

--- namespace.js
+++ namespace.js
@@ -7,7 +7,10 @@ var RAJ00008 = {
         d.addEventListener("click", RAJ00008.Click);//click event
         d.addEventListener("mouseover", RAJ00008.MouseOver);//mouseOver event
         d.addEventListener("mouseout", RAJ00008.MouseOut);//mouseout event
-        boxes.appendChild(d);
+        boxes.appendChild(d);//append todiv
+//       function Click(ev){
+//           ev.currentTarget.classList.toggle("highlight");
+//       }
 //       function MouseOver(ev)
 //       {
 //           ev.currentTarget.classList.toggle("highlight");
@@ -16,14 +19,17 @@ var RAJ00008 = {
 //           ev.currentTarget.classList.toggle("highlight");
 //       }
     },
+    //click function
     Click: function (ev) {
         ev.currentTarget.style.borderColor = "#7E4B7F";//bodercolour changed
         ev.currentTarget.style.backgroundColor = "#FC8AFF";//background changed
     },
+    //MouseOver function
     MouseOver: function (ev) {
-        ev.currentTarget.classList.toggle("highlight");
+        ev.currentTarget.classList.toggle("highlight");//toggel highlight class of CSS
     },
+    //MouseOut function
     MouseOut: function (ev) {
-        ev.currentTarget.classList.toggle("highlight");
+        ev.currentTarget.classList.toggle("highlight");//toggel highlight class of CSS
     }
 };
